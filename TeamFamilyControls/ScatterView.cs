848126fb611b3abc2e56ac67bd03b1fbc4ba605f
Gian Paolo Santopaolo <g_santopaolo@hotmail.com>
2016-01-19 15:40:41 +0100

    initial pull

---
+++ TeamFamilyControls/ScatterView.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace TeamControls
+{
+    [TemplatePart(Name = PART_ROOT_NAME, Type = typeof(Canvas))]
+    public sealed class ScatterView : ItemsControl
+    {
+        private const string PART_ROOT_NAME = "PART_ROOT";
+
+        private Canvas _canvas;
+
+        public ScatterView()
+        {
+            this.DefaultStyleKey = typeof(ScatterView);
+            //this.PointerPressed += ScatterView_PointerPressed;
+        }
+
+        private void ScatterView_PointerPressed(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            
+        }
+
+        private static int Zindex = 0;
+
+        
+
+        //private void MyControl_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        //{
+        //    if (((FrameworkElement)sender).TemplatedParent == null) return;
+        //    ContentPresenter presenter = (ContentPresenter)((FrameworkElement)sender).TemplatedParent;
+
+        //    int currentValue = Panel.GetZIndex(presenter);
+        //    Console.WriteLine(currentValue);
+        //    Panel.SetZIndex(presenter, Zindex++);
+        //}
+
+        public bool AllowItemsRotation
+        {
+            get { return (bool)GetValue(AllowItemsRotationProperty); }
+            set { SetValue(AllowItemsRotationProperty, value); }
+        }
+        public static readonly DependencyProperty AllowItemsRotationProperty =
+           DependencyProperty.Register(nameof(AllowItemsRotation), typeof(bool), typeof(ScatterView), new PropertyMetadata(false));
+
+        protected override void OnItemContainerStyleChanged(Style oldItemContainerStyle, Style newItemContainerStyle)
+        {
+            base.OnItemContainerStyleChanged(oldItemContainerStyle, newItemContainerStyle);
+        }
+
+        protected override void OnItemsChanged(object e)
+        {
+            base.OnItemsChanged(e);
+        }
+
+        protected override void OnItemContainerStyleSelectorChanged(StyleSelector oldItemContainerStyleSelector, StyleSelector newItemContainerStyleSelector)
+        {
+            base.OnItemContainerStyleSelectorChanged(oldItemContainerStyleSelector, newItemContainerStyleSelector);
+        }
+
+        protected override void OnItemTemplateChanged(DataTemplate oldItemTemplate, DataTemplate newItemTemplate)
+        {
+            base.OnItemTemplateChanged(oldItemTemplate, newItemTemplate);
+        }
+
+        protected override void OnItemTemplateSelectorChanged(DataTemplateSelector oldItemTemplateSelector, DataTemplateSelector newItemTemplateSelector)
+        {
+            base.OnItemTemplateSelectorChanged(oldItemTemplateSelector, newItemTemplateSelector);
+        }
+
+        protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+        {
+            base.PrepareContainerForItemOverride(element, item);
+            if(element is FrameworkElement)
+                ((FrameworkElement)element).Loaded += Element_Loaded;
+        }
+
+        private void Element_Loaded(object sender, RoutedEventArgs e)
+        {
+            var element = sender as FrameworkElement;
+
+
+            if (element != null && _canvas != null)
+            {
+                var manManager = new ManipulationManager(element, _canvas);
+
+                manManager.OnFilterManipulation = ManipulationFilter.Clamp;
+                manManager.Configure(true, AllowItemsRotation, true, true);
+
+                element.Loaded -= Element_Loaded;
+            }
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            
+            _canvas = GetTemplateChild(PART_ROOT_NAME) as Canvas;
+            InitEvents();
+        }
+
+        private void InitEvents()
+        {
+            if (_canvas != null)
+                _canvas.SizeChanged += canvas_SizeChanged;
+        }
+
+        private void canvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // Update clipping geometry to its new size
+            // That way contained elements will continue stay at the same relative position after the resize
+            if (_canvas != null)
+            {
+                _canvas.Clip = new Windows.UI.Xaml.Media.RectangleGeometry
+                {
+                    Rect = new Windows.Foundation.Rect(0, 0, _canvas.ActualWidth, _canvas.ActualHeight)
+                };
+            }
+        }
+    }
+}

4bbcf8d40ae17d00dbc9b85b5ad15b8630f776af
Gian Paolo Santopaolo <g_santopaolo@hotmail.com>
2016-01-26 17:40:07 +0100

    added interactioncapability

---
+++ MPCExtensions/Common/InteractionCapability.cs
@@ -0,0 +1,23 @@
+﻿namespace MPCExtensions.Common
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum InteractionCapability
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        MouseAndKeyboard,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        SingleUserMultitouch,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        MultiUserMultitouch
+    }
+}
